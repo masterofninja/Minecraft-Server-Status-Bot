7e0a4e31fc1cf7a08399339511e6b54f0380180d
LEGENDX <68699260+LOG-LEGENDX@users.noreply.github.com>
2021-03-13 22:32:00 +0530

    Added Guild Create Auto Message Event

--- index.js
+++ index.js
@@ -250,6 +250,51 @@ client.on('ready', () => {
 
 })
 
+client.on('guildCreate', guild => {
+
+    let found = 0;
+
+    guild.channels.cache.map((channel) => {
+
+        if (found === 0) {
+
+            if (channel.type === "text") {
+
+                if (channel.permissionsFor(client.user).has("VIEW_CHANNEL") === true) {
+
+                    if (channel.permissionsFor(client.user).has("SEND_MESSAGES") === true) {
+
+                        if (channel.permissionsFor(client.user).has("EMBED_LINKS") === true) {
+
+                            let embedGC = new Discord.MessageEmbed();
+                            embedGC.setTitle("Minecraft Server Status")
+                            embedGC.setURL("https://top.gg/bot/802868654957789204")
+                            embedGC.setDescription(`
+        Thanks for adding me to your server , To Get List Of All Commands Run **.help** Command .
+
+        For Setting Up The Bot Run **.setup** Command , This Command Setups Your Server Ip and Port , Correct Way Of Using This Command Is **.setup ip port** .
+
+        After Setup Run **.status** Command To Get Your Minecraft Server Status .
+
+        For Reset The Ip and Port Run **.reset** Command and Run **.setup** Command Again For Setup The Bot.
+
+        If You Need Any Help With Minecraft Server Status Bot Feel Free To Join Minecraft Server Status **[Support Server](https://discord.gg/EtCsyts)** To Get Your Answers .
+        
+        `)
+                            embedGC.setColor("BLUE");
+                            embedGC.setFooter(`${client.user.tag} By LegendX#8483`, client.user.displayAvatarURL());
+                            embedGC.setTimestamp();
+                            channel.send(embedGC);
+
+                            found = 1;
+                        }
+                    }
+                }
+            }
+        }
+    });
+})
+
 // Bot Commands Setup Begins
 client.on('message', async message => {
 
